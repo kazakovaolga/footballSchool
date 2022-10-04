17ba8a4dc3019fc8eec43fe0970dbcdfb5318803
Olga Kazakova <olegenok@bk.ru>
2022-10-04 16:42:32 +0300

    change webpack entry

--- webpack.config.ts
+++ webpack.config.ts
@@ -13,7 +13,7 @@ const NODE_ENV = process.env.NODE_ENV as
 const GITHUB_PREFIX = '';
 
 const config: webpack.Configuration = {
-  entry: { main: './src/index.tsx' },
+  entry: { bundle: './src/index.tsx' },
   output: {
     path: resolve(__dirname, 'dist'),
     filename: "[name].js",
@@ -36,38 +36,11 @@ const config: webpack.Configuration = {
       {
         test: /\.(css|scss)$/i,
         use: [
-          {
-            loader: "style-loader",
-          },
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 1,
-              modules: {
-                mode: "icss",
-              },
-            },
-          },
-          {
-            loader: "sass-loader",
-          },
-          // MiniCssExtractPlugin.loader,
-          // "css-loader",
-          // { loader: "sass-loader", options: { sourceMap: true } },
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          { loader: "sass-loader", options: { sourceMap: true } },
         ],
-      },
-      // {
-      //   test: /\.s[ac]ss$/i,
-      //   use: ["style-loader", "css-loader", "sass-loader"],
-      // },
-      // {
-      //   test: /\.css$/,
-      //   use: ["style-loader", "css-loader"],
-      // },
-      // {
-      //   test: /\.(css|scss)$/i,
-      //   use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      // },     
+      },    
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         type: 'asset/resource',
