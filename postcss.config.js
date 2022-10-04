c4dc8099870bd9e8da2622f5efb56e4d6e368398
Olga Kazakova <olegenok@bk.ru>
2022-10-04 16:12:09 +0300

    add postcss config

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    parser: "postcss-scss",
+    plugins: {
+      "postcss-import": {},
+      "postcss-preset-env": { browsers: "last 2 versions" },
+      cssnano: {},
+    },
+  };
+  
